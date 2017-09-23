b5b2034154eefe05f1558fdadfad57f3758d232b
Asean12 <gericeser@gmail.com>
2017-09-23 11:48:19 +0800

    Added Comments

--- src/com/Calculator/Main.java
+++ src/com/Calculator/Main.java
@@ -4,32 +4,38 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // write your code here
+
+        //booleans for the while loop
         boolean tester = true;
         String Ans;
 
         while (tester){
 
+            //Start of the Program
 
             System.out.println("Would you like to start the Calculator (Y/N)");
             Scanner Answer = new Scanner(System.in);
+            //converts Scanner output to string
             Ans = Answer.next();
 
+
+            //if ans = "Y" then continue loop
             if (Ans.equals("Y")) {
                 int VarA;
                 int VarB;
                 String UserChoice;
 
+                //First Variable
                 System.out.println("Please insert the first variable.");
                 Scanner Number1 = new Scanner(System.in);
                 VarA = Number1.nextInt();
 
-
+                //Second Variable
                 System.out.println("Please insert the second variable.");
                 Scanner Number2 = new Scanner(System.in);
                 VarB = Number2.nextInt();
 
-
+                //Operation
                 System.out.println("Please pick the Operation (Add, Sub, Mul, Div)");
                 Scanner Operation = new Scanner(System.in);
                 UserChoice = Operation.nextLine();
@@ -56,6 +62,8 @@ public class Main {
                 }
             }
 
+
+            //If Ans = "N" then Close Program
             if (Ans.equals("N")) {
                 tester = false;
                 Answer.close();
